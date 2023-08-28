5d8fa1b6be7c18b9e702d8c2d1e485fc780a0406
Abdul Kareem <rajab8321@gmail.com>
2023-08-28 12:28:25 +0530

    fiest comiit

---
+++ p1.js
@@ -0,0 +1,112 @@
+console.log('file is connected')
+
+// a = 10
+// b = 10.5
+// c = 1055.55555555555555555555555
+// d= 'a'
+// f='dfhshkjsdkaha'
+// g = false
+
+
+// console.log(typeof arr) // replace g with a,b,c,d,f for understanding
+
+
+// arr = [1,2,3,4,5,6,'edad']
+//keys 0,1,2,3,4,5,6
+// console.log(arr[5])
+
+// obj = {
+//     'name':'abdul',
+//     'age':26,
+//     'gender':'male'
+// }
+// console.log(obj['gender'])
+// console.log(obj.name)
+
+
+// let a = ' '+10+5+9
+// console.log(a)
+// a = 10-5
+// a = 10*5
+// a = 10/5
+// a = 10%5
+// a = 10**5
+
+a = 10
+b = 5
+c = [10]
+// console.log(false||true||false)
+// console.log(!false)
+
+// const box = document.getElementById('num')
+
+
+// let timer = setInterval(() => {
+//     box.innerText = parseInt(box.innerText) + 1
+
+
+// }, 100);
+
+// setTimeout(() => {
+//     clearInterval(timer)
+// }, 2000);
+
+
+// console.log('hello')
+// age = 61
+// if (age>=18 && age<=60) {
+//     // code
+//     console.log("you can drive a cae")
+// }
+// else if(age>=60){
+//     console.log('plzz drive a carcarefully')
+// }
+// else{
+//     console.log('you are to young or to old')
+// }
+// let key = prompt("enter a number")
+// key = parseInt(key)
+// switch (key) {
+//     case 1:
+//         console.log("print one")
+        
+//         break;
+//     case 2:
+//         console.log("print two")
+        
+//         break;
+
+//     default:
+//         console.log("number is larger")
+//         break;
+// }
+
+
+// for(let i=0;i<10;i++)
+// {
+//     console.log(i)
+// }
+
+// let i=0;
+// while(i<10)
+// {
+//     console.log(i)
+//     i++;
+// }
+
+// let i= 11
+// do {
+//     console.log(i)
+// } while (i<10);
+
+function sum (a,b){
+    return a * b;
+}
+
+
+console.log(sum(10,40))
+console.log(sum(20,40))
+console.log(sum(30,40))
+console.log(sum(10,40))
+console.log(sum(10,40))
+console.log(sum(10,40))
